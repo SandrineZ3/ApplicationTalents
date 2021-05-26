e2db20842f22d9b107be65a1a2eda1b24cb511da
noams88-dev <73168161+noams88-dev@users.noreply.github.com>
2021-05-26 12:46:07 +0200

    Création Interpersonnelle

---
+++ src/Controller/InterpersonnelleController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Interpersonnelle;
+use App\Form\InterpersonnelleFormType;
+use App\Repository\InterpersonnelleRepository;
+use App\Repository\LevelOfDifficultyRepository;
+use App\Repository\UserRepository;
+use App\Services\InterpersonnelleUtils;
+use App\Services\Utils;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InterpersonnelleController extends AbstractController
+{
+    /**
+     * @Route("/interpersonnelle", name="interpersonnelle")
+     */
+    public function show(Request $request, EntityManagerInterface $entityManager, InterpersonnelleUtils $interpersonnelleUtils, Utils $utils, UserRepository $userRepository, LevelOfDifficultyRepository $levelOfDifficultyRepository, InterpersonnelleRepository $interpersonnelleRepository): Response
+    {
+        $user = $userRepository->find($this->getUser());
+        if ($user->getInterpersonnelleFinished()) {
+            return $this->redirectToRoute('main');
+        }
+
+        $tableauReponse = ['reponseFacile', 'reponseMoyen', 'reponseDifficile'];
+        foreach ($tableauReponse as $index => $reponseUser) {
+            if ($request->get($reponseUser)) {
+                if ($index === 0) {
+                    $user->setScoreInterpersonnelle(0);
+                    $entityManager->flush();
+                }
+
+                $interpersonnelleUtils->recordScore($request, $reponseUser, $interpersonnelleRepository, $user, $entityManager);
+
+                if ($index === 0 || $index === 1) {
+                    $enigmeRandom = $utils->nextEnigme($index + 2, $interpersonnelleRepository, $levelOfDifficultyRepository);
+                    $nomInput = $tableauReponse[$index + 1];
+                    return new JsonResponse([
+                        'content' => $this->renderView('interpersonnelle/content/formEnigme.html.twig', compact('enigmeRandom', 'nomInput'))
+                    ]);
+                }
+                else {
+                    return new JsonResponse([
+                        'content' => $this->renderView('interpersonnelle/content/endScreen.html.twig')
+                    ]);
+                }
+            }
+        }
+        $enigmeRandom = $utils->nextEnigme(1, $interpersonnelleRepository, $levelOfDifficultyRepository);
+        $nomInput = 'reponseFacile';
+
+        return $this->render('interpersonnelle/show.html.twig', [
+            'enigmeRandom' => $enigmeRandom,
+            'nomInput' => $nomInput
+        ]);
+    }
+
+    /**
+     * @Route("admin/interpersonnelle/create", name="interpersonnelle_create")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $interpersonnelle = new Interpersonnelle();
+        $interpersonnelleForm = $this->createForm(InterpersonnelleFormType::class, $interpersonnelle);
+        $interpersonnelleForm = $interpersonnelleForm->handleRequest($request);
+
+        if ($interpersonnelleForm->isSubmitted() && $interpersonnelleForm->isValid()) {
+            $entityManager->persist($interpersonnelle);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'énigme a bien été enregistré');
+            return $this->redirectToRoute('interpersonnelle_create');
+        }
+
+        return $this->render('interpersonnelle/create.html.twig', [
+            'interpersonnelleForm' => $interpersonnelleForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("admin/interpersonnelle/{id}/update", name="interpersonnelle_update", requirements={"id"="\d+"})
+     */
+    public function update(int $id, Request $request, EntityManagerInterface $entityManager, InterpersonnelleRepository $interpersonnelleRepository): Response
+    {
+        $interpersonnelle = $interpersonnelleRepository->find($id);
+        if (!$interpersonnelle) {
+            $this->addFlash('error', 'L\'enigme recherchée n\'existe pas');
+            return $this->redirectToRoute('main');
+        }
+
+        $interpersonnelleForm = $this->createForm(InterpersonnelleFormType::class, $interpersonnelle);
+        $interpersonnelleForm = $interpersonnelleForm->handleRequest($request);
+
+        if ($interpersonnelleForm->isSubmitted() && $interpersonnelleForm->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'énigme a bien été mise à jour');
+            return $this->redirectToRoute('interpersonnelle_update', ["id" => $id]);
+        }
+
+        return $this->render('interpersonnelle/update.html.twig', [
+            'interpersonnelleForm' => $interpersonnelleForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("admin/interpersonnelle/{id}/delete", name="interpersonnelle_delete", requirements={"id"="\d+"})
+     */
+    public function delete(int $id, EntityManagerInterface $entityManager, InterpersonnelleRepository $interpersonnelleRepository): Response
+    {
+        $interpersonnelle = $interpersonnelleRepository->find($id);
+        if (!$interpersonnelle) {
+            $this->addFlash('error', 'L\'enigme recherchée n\'existe pas');
+            return $this->redirectToRoute('main');
+        }
+
+        $entityManager->remove($interpersonnelle);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'L\'énigme a bien été supprimée');
+        return $this->redirectToRoute('main');
+    }
+}
---
+++ src/Form/InterpersonnelleFormType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Emoticon;
+use App\Entity\Interpersonnelle;
+use App\Entity\LevelOfDifficulty;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class InterpersonnelleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('urlVideo', TextType::class, [
+                'label' => 'Lien vidéo Youtube',
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner l\'url de la vidéo',
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Votre pseudo doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('solution', EntityType::class, [
+                'class' => Emoticon::class,
+                'required' => false,
+                'expanded' => true,
+                'multiple' => false,
+                'placeholder' => false,
+                'choice_label' => 'label',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner la solution',
+                    ]),
+                ],
+            ])
+            ->add('levelOfDifficulty', EntityType::class, [
+                'class' => LevelOfDifficulty::class,
+                'label' => 'Difficulté',
+                'required' => false,
+                'expanded' => false,
+                'multiple' => false,
+                'placeholder' => 'Choisir la difficulté',
+                'choice_label' => 'label',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner la difficulté',
+                    ]),
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Interpersonnelle::class,
+        ]);
+    }
+}
