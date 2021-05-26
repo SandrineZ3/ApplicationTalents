385a5272aa9d895fc845bd08940643a5820187c9
noams88-dev <73168161+noams88-dev@users.noreply.github.com>
2021-05-26 08:30:20 +0200

    Factoring Naturaliste + VisuoSpatiale

--- src/Services/Utils.php
+++ src/Services/Utils.php
@@ -2,10 +2,21 @@
 
 namespace App\Services;
 
+use App\Repository\LevelOfDifficultyRepository;
+
 class Utils
 {
-    public function saveImageAndGenerateUrl($naturalisteForm, string $name, string $directory): string {
-        $image = $naturalisteForm->get($name)->getData();
+    public function nextEnigme(int $levelOfDifficultySuivant, $repository, LevelOfDifficultyRepository $levelOfDifficultyRepository)
+    {
+        $levelOfDifficulty = $levelOfDifficultyRepository->find($levelOfDifficultySuivant);
+        $tableauEnigme = $repository->findBy(array('levelOfDifficulty' => $levelOfDifficulty), array('id' => 'ASC'), null, 0);
+        $indexRandom = array_rand($tableauEnigme);
+
+        return $tableauEnigme[$indexRandom];
+    }
+
+    public function saveImageAndGenerateUrl($form, string $name, string $directory): string {
+        $image = $form->get($name)->getData();
         $urlImage = md5(uniqid()) . '.' . $image->guessExtension();
         $image->move($directory, $urlImage);
         return $urlImage;
