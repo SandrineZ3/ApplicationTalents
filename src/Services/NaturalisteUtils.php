f9079c65ec79b6ddd775a1d714583208866d8d0b
noams88-dev <73168161+noams88-dev@users.noreply.github.com>
2021-06-01 19:13:29 +0200

    Début mise en forme

--- src/Services/NaturalisteUtils.php
+++ src/Services/NaturalisteUtils.php
@@ -12,9 +12,12 @@ class NaturalisteUtils
     public function recordScore(Request $request, string $reponse, NaturalisteRepository $naturalisteRepository, User $user, EntityManagerInterface $entityManager): void
     {
         $enigme = $naturalisteRepository->find($request->get("idEnigme"));
+        $tableauBonneReponse = explode(',', $enigme->getSolution());
+        $tableauBonneReponse_trim = array_map('trim', $tableauBonneReponse);
+        $tableauBonneReponse_trim_lower = array_change_key_case($tableauBonneReponse_trim, CASE_LOWER);
 
         // On cherche si la réponse donnée par l'user figure dans la string des solutions acceptées
-        if (strpos($enigme->getSolution(), $reponse) !== false) {
+        if (in_array(strtolower(trim($reponse)), $tableauBonneReponse_trim_lower)) {
             $pointsGagnes = $enigme->getLevelOfDifficulty()->getPoints();
             $currentScore = $user->getScoreNaturaliste();
             $user->setScoreNaturaliste($currentScore + $pointsGagnes);
