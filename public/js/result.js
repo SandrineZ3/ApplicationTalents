a6e0940e0fae0ec510b43724e44f93e5adaf0719
Sandrine Zabo <sandrine.zaborowski2020@campus-eni.fr>
2021-07-02 12:16:52 +0200

    Partage réseaux sociaux

--- public/js/result.js
+++ public/js/result.js
@@ -56,3 +56,41 @@ function generatePDF() {
     };
     html2pdf().set(options).from(element).save();
 }
+
+(function() {
+    let popupCenter = function(url, title, width, height){
+        let popupWidth = width || 640;
+        let popupHeight = height || 320;
+        let windowLeft = window.screenLeft || window.screenX;
+        let windowTop = window.screenTop || window.screenY;
+        let windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        let windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        let popupLeft = windowLeft + windowWidth / 2 - popupWidth / 2 ;
+        let popupTop = windowTop + windowHeight / 2 - popupHeight / 2;
+        let popup = window.open(url, title, 'scrollbars=no, width=' + popupWidth + ', height=' + popupHeight + ', top=' + popupTop + ', left=' + popupLeft);
+        popup.focus();
+        return true;
+    };
+
+    document.querySelector('.ui.circular.twitter.icon.button').addEventListener('click', function(e){
+        e.preventDefault();
+        let url = this.getAttribute('data-url');
+        let shareUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(document.title) +
+            "&url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Twitter");
+    });
+
+    document.querySelector('.ui.circular.facebook.icon.button').addEventListener('click', function(e){
+        e.preventDefault();
+        let url = this.getAttribute('data-url');
+        let shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur facebook");
+    });
+
+    document.querySelector('.ui.circular.linkedin.icon.button').addEventListener('click', function(e){
+        e.preventDefault();
+        let url = this.getAttribute('data-url');
+        let shareUrl = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Linkedin");
+    });
+})();
